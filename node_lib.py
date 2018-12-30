9e3de9d35f67b538d5bd7e570610b37a1fd3a251
rajdakin <rajdakin@gmail.com>
2018-12-30 12:25:17 +0100

    Revert "Changed dictionary parameter to the dictionary parameters conversion" as requested via PM
    This reverts commit 3c64a9053b9e8702a21c368e8da0d7fb06bbfcc5.

--- node_lib.py
+++ node_lib.py
@@ -7,7 +7,7 @@ class Node :
     def update_symbol(self) :
         raise NotImplementedError
 
-    def compute(self, **feed_dict) :
+    def compute(self, feed_dict) :
         raise NotImplementedError
 
     def derivate(self) :
@@ -29,7 +29,7 @@ class Placeholder(Node):
     def update_symbol(self) :
         return self.symbol
 
-    def compute(self, **feed_dict) :
+    def compute(self, feed_dict) :
         try :
             return feed_dict[self.symbol]
         except :
@@ -53,7 +53,7 @@ class Scalar(Node) :
         self.symbol = '(' + str(self.value) + ')'
         return self.symbol
 
-    def compute(self, **feed_dict) :
+    def compute(self, feed_dict) :
         return self.value
 
     def derivate(self, symbol) :
@@ -84,8 +84,8 @@ class Sommator(Operator):
         self.symbol = '(' + self.input1.update_symbol() + "+" + self.input2.update_symbol() + ')'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return self.input1.compute(**feed_dict) + self.input2.compute(**feed_dict)
+    def compute(self, feed_dict) :
+        return self.input1.compute(feed_dict) + self.input2.compute(feed_dict)
 
     def derivate(self, symbol) :
         return Sommator([self.input1.derivate(symbol), self.input2.derivate(symbol)])
@@ -96,8 +96,8 @@ class Substractor(Operator):
         self.symbol = '(' + self.input1.update_symbol() + "-" + self.input2.update_symbol() + ')'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return self.input1.compute(**feed_dict) - self.input2.compute(**feed_dict)
+    def compute(self, feed_dict) :
+        return self.input1.compute(feed_dict) - self.input2.compute(feed_dict)
 
     def derivate(self, symbol) :
         return Substractor([self.input1.derivate(symbol), self.input2.derivate(symbol)])
@@ -107,8 +107,8 @@ class Multiplicator(Operator) :
         self.symbol = '(' + self.input1.update_symbol() + "*" + self.input2.update_symbol() + ')'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return self.input1.compute(**feed_dict) * self.input2.compute(**feed_dict)
+    def compute(self, feed_dict) :
+        return self.input1.compute(feed_dict) * self.input2.compute(feed_dict)
 
     def derivate(self, symbol) :
         return Sommator([Multiplicator([self.input1.derivate(symbol), self.input2]), Multiplicator([self.input1, self.input2.derivate(symbol)])])
@@ -118,8 +118,8 @@ class Divisor(Operator) :
         self.symbol = '(' + self.input1.update_symbol() + "/" + self.input2.update_symbol() + ')'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return self.input1.compute(**feed_dict) / self.input2.compute(**feed_dict)
+    def compute(self, feed_dict) :
+        return self.input1.compute(feed_dict) / self.input2.compute(feed_dict)
 
     def derivate(self, symbol) :
         return Divisor([Substractor([Multiplicator([self.input1.derivate(symbol), self.input2]), Multiplicator([self.input1, self.input2.derivate(symbol)])]), Power([self.input2, Scalar([2])])])
@@ -130,8 +130,8 @@ class Power(Operator) :
         self.symbol = '(' + self.input1.update_symbol() + "^" + self.input2.update_symbol() + ')'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return self.input1.compute(**feed_dict) ** self.input2.compute(**feed_dict)
+    def compute(self, feed_dict) :
+        return self.input1.compute(feed_dict) ** self.input2.compute(feed_dict)
 
     def derivate(self, symbol) :
         return Multiplicator([Power([self.input1, self.input2]), Sommator([Multiplicator([self.input1.derivate(symbol), Divisor([self.input2, self.input1])]), Multiplicator([self.input2.derivate(symbol), LogarithmNeperien([self.input1])])])])
@@ -150,9 +150,9 @@ class Logarithm(Node) :
         self.symbol = '(log[' + self.base.update_symbol() + '](' + self.input.update_symbol() + '))'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        arg_value = self.input.compute(**feed_dict)
-        base_value = self.base.compute(**feed_dict)
+    def compute(self, feed_dict) :
+        arg_value = self.input.compute(feed_dict)
+        base_value = self.base.compute(feed_dict)
         if arg_value > 0 and base_value > 0 and base_value != 1 :
             return np.log(arg_value) / np.log(base_value)
         else :
@@ -177,8 +177,8 @@ class Cos(Function) :
         self.symbol = '(cos(' + self.input.update_symbol() + '))'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return np.cos(self.input.compute(**feed_dict))
+    def compute(self, feed_dict) :
+        return np.cos(self.input.compute(feed_dict))
 
     def derivate(self, symbol) :
         return Multiplicator([Multiplicator([Scalar([-1]), Sin([self.input])]), self.input.derivate(symbol)])
@@ -188,8 +188,8 @@ class Sin(Function) :
         self.symbol = '(sin(' + self.input.update_symbol() + '))'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return np.sin(self.input.compute(**feed_dict))
+    def compute(self, feed_dict) :
+        return np.sin(self.input.compute(feed_dict))
 
     def derivate(self, symbol) :
         return Multiplicator([Cos([self.input]), self.input.derivate(symbol)])
@@ -199,8 +199,8 @@ class Tan(Function) :
         self.symbol = '(tan(' + self.input.update_symbol() + '))'
         return self.symbol
 
-    def compute(self, **feed_dict) :
-        return np.tan(self.input.compute(**feed_dict))
+    def compute(self, feed_dict) :
+        return np.tan(self.input.compute(feed_dict))
 
     def derivate(self, symbol) :
         return Divisor([Sin([self.input]), Cos([self.input])]).derivate(symbol)
@@ -213,9 +213,9 @@ if __name__ == '__main__' :
     feed_dict = {'x':2, 'u':0.2}
 
     print(f1.update_symbol())
-    print(f1.compute(**feed_dict))
+    print(f1.compute(feed_dict))
     print(f1.derivate('u').update_symbol())
-    print(f1.derivate('u').compute(**feed_dict))
+    print(f1.derivate('u').compute(feed_dict))
 
     for i in f1.get_placeholder() :
         print(i.symbol)
